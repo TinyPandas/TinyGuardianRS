caf5d2dbfa8edf22235862aed6dee1e2c2c71cc0
TSP_Panda <chris.barwick16@gmail.com>
2020-08-07 14:34:36 -0500

    Capture all outs
    - modified code to handle joining of user_res and user_err at end of eval
    - eval now returns Lua env_err

--- src/commands/lua.rs
+++ src/commands/lua.rs
@@ -5,7 +5,7 @@ use serenity::{
         Args, CommandResult, macros::{command}
     }
 };
-use rlua::{Lua, Result, Variadic};
+use rlua::{Lua, Result, Variadic, Value::Nil};
 
 #[command]
 #[min_args(1)]
@@ -13,43 +13,88 @@ pub async fn lua(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     let lua = Lua::new();
     let src = args.remains().unwrap_or("print('NoCode')");
     
-    let mut results: Vec<String> = vec![];
-
-    let _: Result<()> = lua.context(|lua_ctx| {
+    let result: Result<(String, String, String)> = lua.context(move |lua_ctx| {
         let globals = lua_ctx.globals();
+        let mut res: Vec<String> = vec![];
+        let mut err: Vec<String> = vec![];
 
-        lua_ctx.scope(|scope| {
+        let eval = lua_ctx.scope(|scope| {
             let func = scope.create_function_mut(|_, strings: Variadic<String>| {
-                results.push(strings.join(" "));
+                res.push(format!("[out]: {}", strings.join(" ")));
                 Ok(())
             })?;
+
+            let err_func = scope.create_function_mut(|_, strings: Variadic<String>| {
+                err.push(format!("[err]: {}", strings.join(" ")));
+                Ok(())
+            })?;
+
+            globals.set("os", Nil)?;
+            globals.set("io", Nil)?;
 
             globals.set(
                 "print",
                 func,
             )?;
 
-            lua_ctx.load(src)
-            .set_name("test")?
-            .exec()?;
+            globals.set(
+                "error",
+                err_func,
+            )?;
 
-            Ok(())
+            let c = lua_ctx.load(src)
+            .set_name("test")?
+            .exec();
+
+            let err_cap = match c {
+                Ok(_) => {
+                    String::from("No errors.")
+                },
+                Err(why) => {
+                    why.to_string()
+                }
+            };
+
+            Ok(err_cap)
         })?;
 
-        Ok(())
+        Ok((res.join("\n"), err.join("\n"), eval))
     });
 
+    
+    let r = match result {
+        Ok(r) => {
+            r
+        }, Err(why) => {
+            (String::from(""), String::from(""), why.to_string())
+        }
+    };
+    let out = r.0.as_str();
+    let user_err = r.1.as_str();
+    let comb = format!("{}\n{}", out, user_err);
+    let mut env_out = comb.as_str();
+    let mut env_err = r.2.as_str();
+
     let nick = msg.author.id.to_string();
-    let res_temp = results.join("\n");
-    let mut res = res_temp.as_str();
-    if res.len() > 1500 {
-        res = &res[1..1500];
+    if env_out.len() > 1500 {
+        env_out = &env_out[1..1500];
+    }
+    if env_err.len() > 1500 {
+        env_err = &user_err[1..1500];
+    }
+
+    if env_out.len() == 0 {
+        env_out = "No output.";
+    }
+    if env_err.len() == 0 {
+        env_err = "No errors.";
     }
 
     let _ = msg.channel_id.send_message(ctx, |m| {
         m.embed(|e| {
             e.title(format!("Code Eval for <@{}>", nick));
-            e.field("Eval", res, false);
+            e.field("Eval", env_out, false);
+            e.field("Errors", env_err, false);
 
             e
         });
