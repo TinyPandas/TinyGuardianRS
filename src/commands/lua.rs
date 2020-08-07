563ec7aafd506e94350c12f04009799b654bdf5d
TSP_Panda <chris.barwick16@gmail.com>
2020-08-06 23:06:09 -0500

    More Sophisticated Methods
    - removed hacky method of storing outs
    - replaced with `lua_ctx.scope`
    - Embeded eval result
    - limited result to 1500 characters.

--- src/commands/lua.rs
+++ src/commands/lua.rs
@@ -5,55 +5,57 @@ use serenity::{
         Args, CommandResult, macros::{command}
     }
 };
-use rlua::{Function, Lua, Result, Variadic};
-use std::{sync::Mutex, collections::HashMap};
-use once_cell::sync::Lazy;
-
-static CAPTURES: Lazy<Mutex<HashMap<String, Vec<String>>>> = Lazy::new(|| {
-    let mut m = HashMap::new();
-    m.insert(String::from("13"), vec![String::from("")]);
-    Mutex::new(m)
-});
+use rlua::{Lua, Result, Variadic};
 
 #[command]
 #[min_args(1)]
 pub async fn lua(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     let lua = Lua::new();
     let src = args.remains().unwrap_or("print('NoCode')");
-    let author_id = msg.author.id.to_string();
-    let env_name = format!("exec_for_{}", author_id);
+    
+    let mut results: Vec<String> = vec![];
 
     let _: Result<()> = lua.context(|lua_ctx| {
         let globals = lua_ctx.globals();
-        let _old_print = globals.get::<_, Function>("print").unwrap();
 
-        let new_print = lua_ctx.create_function(|_, strings: Variadic<String>| {
-            let mut cap = CAPTURES.lock().unwrap();
-            let mut vec = cap.get(&String::from("13")).unwrap().clone();
-            vec.insert(vec.len(), strings.join(" "));
-            cap.insert(String::from("13"), vec);
+        lua_ctx.scope(|scope| {
+            let func = scope.create_function_mut(|_, strings: Variadic<String>| {
+                results.push(strings.join(" "));
+                Ok(())
+            })?;
+
+            globals.set(
+                "print",
+                func,
+            )?;
+
+            lua_ctx.load(src)
+            .set_name("test")?
+            .exec()?;
+
             Ok(())
         })?;
-        globals.set("print", new_print)?;
-
-        lua_ctx.load(src)
-        .set_name(&env_name)?
-        .exec()?;
 
         Ok(())
     });
 
+    let nick = msg.author.id.to_string();
+    let res_temp = results.join("\n");
+    let mut res = res_temp.as_str();
+    if res.len() > 1500 {
+        res = &res[1..1500];
+    }
+
     let _ = msg.channel_id.send_message(ctx, |m| {
-        let cap = CAPTURES.lock().unwrap();
-        let vec = cap.get(&String::from("13")).unwrap();
-        let result = vec.join(" ");
-        m.content(result);
+        m.embed(|e| {
+            e.title(format!("Code Eval for <@{}>", nick));
+            e.field("Eval", res, false);
+
+            e
+        });
 
         m
     }).await;
-
-    let mut cap = CAPTURES.lock().unwrap();
-    cap.insert(String::from("13"), vec![]);
 
     Ok(())
 }
